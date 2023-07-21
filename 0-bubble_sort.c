dd5b1f24dc3f7f9725eaf9e42b141c5a7c84eedb
anasyaser <anoos649@gmail.com>
2023-07-21 22:34:21 +0300

    Fix bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,12 +11,13 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
-	size_t num_swaps = 0;
+	size_t j;
+	int swaps = 0;
 	int tmp;
 
-	while (1)
+	for (j = 0; j < size - 1; j++)
 	{
-		for (i = 0; i < size - 1; i++)
+		for (i = 0; i < size - j - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
@@ -24,7 +25,7 @@ void bubble_sort(int *array, size_t size)
 				array[i] = array[i + 1];
 				array[i + 1] = tmp;
 				print_array(array, size);
-				num_swaps += 1;
+				swaps = 1;
 			}
 		}
 		if (num_swaps == 0)
