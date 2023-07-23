a34d9ff61986c295aeabc75b1c4c8d36a3ccb484
anasyaser <anoos649@gmail.com>
2023-07-23 19:18:25 +0300

    Fix selection sort

--- 1-main.c
+++ 1-main.c
@@ -39,9 +39,9 @@ listint_t *create_listint(const int *array, size_t size)
 int main(void)
 {
 	listint_t *list;
+	
 	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 	size_t n = sizeof(array) / sizeof(array[0]);
-
 	list = create_listint(array, n);
 	if (!list)
 		return (1);
@@ -50,5 +50,8 @@ int main(void)
 	insertion_sort_list(&list);
 	printf("\n");
 	print_list(list);
+	printf("---------");
+
+	
 	return (0);
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,25 +11,26 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i;
-	size_t o;
+	size_t j;
 	int tmp;
 	size_t min_idx;
 
-	for (o = 0; o < size - 1; o++)
+	if (size < 2)
+		return;
+	for (j = 0; j < size - 1; j++)
 	{
-		min_idx = o;
-		for (i = o + 1; i < size; i++)
+		min_idx = j;
+		for (i = j + 1; i < size; i++)
 		{
 			if (array[i] < array[min_idx])
 				min_idx = i;
-
-			if (min_idx != o)
-			{
-				tmp = array[o];
-				array[o] = array[min_idx];
-				array[min_idx] = tmp;
-			}
 		}
-		print_array(array, size);
+		if (min_idx != j)
+		{
+				tmp = array[j];
+				array[j] = array[min_idx];
+				array[min_idx] = tmp;
+				print_array(array, size);
+		}
 	}
 }
