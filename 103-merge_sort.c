22eed0bd09e70d931e44ec2e25ab9c1a1929cc58
anasyaser <anoos649@gmail.com>
2023-09-07 18:08:23 +0300

    merge_sort v0

---
+++ 103-merge_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * merge - merge two array in sorted order
+ *
+ * @array: array of numbers to sort
+ * @aux_array: addtional array to store sorted numbers
+ * @l: lower bound of array1
+ * @m: index of end of array1
+ * @h: higher bound of array2
+ */
+void  merge(int *array, int *aux_array, size_t l,size_t m,  size_t h)
+{
+	
+}
